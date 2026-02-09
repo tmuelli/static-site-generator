69f9c1e42d50df8299e039873956a6247e9722e7
tmuelli <tm.muellertom@gmail.com>
2026-02-09 07:44:27 +0100

    DEV: added more conversion functions

--- src/functions.py
+++ src/functions.py
@@ -1,6 +1,35 @@
+import re
+
 from leafnode import LeafNode
 from textnode import TextNode, TextType
+from blocktype import BlockType
+from parentnode import ParentNode
 
+#########################################################################
+# Function: extract_markdown_images - extract alt and url from markdown #
+#                                     which contains an image string    #
+# Input:    text - markdown text                                        #
+# Return:   tuple of matches (e.g. (alt, url))                          #
+#########################################################################
+def extract_markdown_images(text):
+    matches = re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    return matches
+
+##############################################################################
+# Function: extract_markdown_links - extract link text and url from markdown #
+#                                    which contains an link string           #
+# Input:    text_node - markdown text                                        #
+# Return:   tuple of matches (e.g. (link_text, link_url))                    #
+##############################################################################
+def extract_markdown_links(text):
+    matches = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    return matches
+
+###############################################################
+# Function: text_to_html_node - from TextNode create LeafNode #
+# Input:    text_node - TextNode to convert                   #
+# Return:   LeafNode                                          #
+###############################################################
 def text_to_html_node(text_node):
     match (text_node.text_type):
         case TextType.PLAIN:
@@ -24,15 +53,333 @@ def text_to_html_node(text_node):
         case _:
             raise Exception("Invalid text node type.")
 
+##################################################################
+# Function: split_nodes_delimiter - split TextNodes by delimiter #
+# Input:    old_node  - list of old TextNodes                    #
+#           delimiter - delimiter by which list is split         #
+#           text_type - type of TextNode (matching delimiter)    #
+# Return:   list of new splitted TextNodes                       #
+##################################################################
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     result_nodes = []
 
     for node in old_nodes:
         if node.text_type != TextType.PLAIN:
-            result_nodes.append(node.text_type)
+            result_nodes.append(node)
             continue
 
-        # split nodes
-        
+        # split nodes by delimiter
+        seperate_values = node.text.split(delimiter)
+        num_values = len(seperate_values)
+        if (num_values % 2) == 0:
+            raise SyntaxError("Invalid markdown syntax!")
+
+        for i in range(num_values):
+            if (i % 2) != 0:
+                result_nodes.append(
+                    TextNode(seperate_values[i], text_type)
+                )
+            else:
+                result_nodes.append(
+                    TextNode(seperate_values[i], TextType.PLAIN)
+                )
 
     return result_nodes
+
+##################################################################
+# Function: split_nodes_images - split image TextNode into nodes #
+#                                for text and image              #
+# Input:    old_node  - list of old TextNodes                    #
+# Return:   list of new splitted TextNodes for image             #
+##################################################################
+def split_nodes_images(old_nodes):
+    result_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.PLAIN:
+            result_nodes.append(node)
+            continue
+
+        orgiginal_text = node.text
+        img_matches = extract_markdown_images(orgiginal_text)
+
+        for alt, url in img_matches:
+            sections = orgiginal_text.split(f"![{alt}]({url})", 1)
+            if sections[0] != "":
+                result_nodes.append(
+                    TextNode(sections[0], TextType.PLAIN)
+                )
+
+            result_nodes.append(TextNode(alt, TextType.IMAGE, url))
+
+            orgiginal_text = sections[1]
+
+        if orgiginal_text != "":
+            result_nodes.append(TextNode(orgiginal_text, TextType.PLAIN))
+
+    return result_nodes
+
+##################################################################
+# Function: split_nodes_links - split link TextNode into nodes   #
+#                                for text and link               #
+# Input:    old_node  - list of old TextNodes                    #
+# Return:   list of new splitted TextNodes for links             #
+##################################################################
+def split_nodes_links(old_nodes):
+    result_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.PLAIN:
+            result_nodes.append(node)
+            continue
+
+        orgiginal_text = node.text
+        img_matches = extract_markdown_links(orgiginal_text)
+
+        for alt, url in img_matches:
+            sections = orgiginal_text.split(f"[{alt}]({url})", 1)
+            if sections[0] != "":
+                result_nodes.append(
+                    TextNode(sections[0], TextType.PLAIN)
+                )
+
+            result_nodes.append(TextNode(alt, TextType.LINK, url))
+
+            orgiginal_text = sections[1]
+
+        if orgiginal_text != "":
+            result_nodes.append(TextNode(orgiginal_text, TextType.PLAIN))
+
+    return result_nodes
+
+#############################################################################
+# Function: text_to_textnodes - converts text into list of textnodes        #
+#                               depending on delimiter / type (image, link) #
+# Input:    text - text to convert                                          #
+# Return:   list of TextNodes after splitting                               #
+#############################################################################
+def text_to_textnodes(text):
+    result_nodes = [TextNode(text, TextType.PLAIN)]
+
+    result_nodes = (split_nodes_delimiter(result_nodes, '**', TextType.BOLD))
+    result_nodes = (split_nodes_delimiter(result_nodes, '_', TextType.ITALIC))
+    result_nodes = (split_nodes_delimiter(result_nodes, '`', TextType.CODE))
+    result_nodes = (split_nodes_images(result_nodes))
+    result_nodes = (split_nodes_links(result_nodes))
+
+    return result_nodes
+
+###################################################################
+# Function: markdown_to_blocks - converts single markdown string  #
+#                                into list of markdown blocks     #
+#                                (split by newlines)              #
+# Input:    markdown - markdown string                            #
+# Return:   list of markdown blocks (strings splitted by newline) #
+###################################################################
+def markdown_to_blocks(markdown):
+    blocks = []
+    lines = markdown.split("\n\n")
+    for line in lines:
+        if line == "":
+            continue
+
+        blocks.append(line.strip())
+    return blocks
+
+######################################################################
+# Function: block_to_block_type - get block type from markdown block #
+# Input:    markdown - markdown block                                #
+# Return:   BlockType of markdown block                              #
+######################################################################
+def block_to_block_type(markdown):
+    b_type = BlockType.PARAGRAPH
+
+    # headings
+    if markdown.startswith('#'):
+        count = 0
+        for i in range(len(markdown)):
+            if markdown[i] == ' ':
+                break
+
+            if count >= 6:
+                break
+
+            if markdown[i] == '#':
+                count += 1
+                continue
+
+            break
+
+        if count < len(markdown) and count >= 1 and count <=6:
+            if markdown[count] == ' ':
+                return BlockType.HEADING
+
+        return BlockType.PARAGRAPH
+
+    # code blocks
+    if markdown.startswith('```\n') and markdown.endswith('```'):
+        return BlockType.CODE
+
+    # quotes
+    if markdown.startswith('>'):
+        return BlockType.QUOTE
+
+    # unordered list
+    if markdown.startswith('- '):
+        list_entries = markdown.split('\n')
+        is_valid_ulist = True
+        for entry in list_entries:
+            if not entry.startswith('- '):
+                is_valid_ulist = False
+                break
+
+        return BlockType.UNORDERED_LIST if is_valid_ulist else BlockType.PARAGRAPH
+
+    # ordered list
+    if markdown.startswith('1. '):
+        list_entries = markdown.split('\n')
+        is_valid_olist = True
+        for i in range(len(list_entries)):
+            if not list_entries[i].startswith(f"{i+1}. "):
+                is_valid_olist = False
+
+        return BlockType.ORDERED_LIST if is_valid_olist else BlockType.PARAGRAPH
+
+    return b_type
+
+
+########################################################################
+# Function: text_to_children - converts markdown text to html children #
+# Input:    text - text to convert                                     #
+# Return:   list of html children                                      #
+########################################################################
+def text_to_children(text):
+    html_children = []
+    text_nodes = text_to_textnodes(text)
+    for text_node in text_nodes:
+        html_children.append(text_to_html_node(text_node))
+    return html_children
+
+########################################################################
+# Function: prepare_heading_block - trim block from # symbols and      #
+#                                   and count level of heading         #
+# Input:    block - text of block                                      #
+# Return:   trimmed block text                                         #
+#           level of heading                                           #
+########################################################################
+def prepare_heading_block(block):
+    trimmed = ""
+    level = 0
+
+    # count consecutive hashtag characters at the beginning of the block text
+    for i in range(len(block)):
+        if block[i] == ' ':
+            break
+        if level >= 6:
+            break
+        if block[i] == '#':
+            level += 1
+
+    if level >= len(block) or level < 1 or level > 6:
+        raise ValueError("Invalid markdown heading!")
+
+    trimmed = block[level+1:]
+    return trimmed, level
+
+########################################################################
+# Function: prepare_quote_block - remove markdown quote character and  #
+#                                 whitespaces from quotes              #
+# Input:    block - text of block                                      #
+# Return:   trimmed block text                                         #
+########################################################################
+def prepare_quote_block(block):
+    lines = block.split('\n')
+    result = []
+    for line in lines:
+        result.append(line[1:].strip())
+    return '\n'.join(result)
+
+#############################################################################
+# Function: prepare_unordered_list_block - get all list children for block  #
+# Input:    block - text of block                                           #
+# Return:   list of list childrens                                          #
+#############################################################################
+def prepare_unordered_list_block(block):
+    lines = block.split('\n')
+    li_children = []
+    for line in lines:
+        text = line[2:].strip()
+        elem_children = text_to_children(text)
+        li_children.append(ParentNode("li", elem_children))
+    return li_children
+
+#############################################################################
+# Function: prepare_ordered_list_block - get all list children for block    #
+# Input:    block - text of block                                           #
+# Return:   list of list childrens                                          #
+#############################################################################
+def prepare_ordered_list_block(block):
+    lines = block.split('\n')
+    li_children = []
+    for i, line in enumerate(lines):
+        prefix_len = len(f"{i+1}. ")
+        elem_children = text_to_children(line[prefix_len:])
+        li_children.append(ParentNode("li", elem_children))
+    return li_children
+
+
+####################################################################
+# Function: block_to_html - converts markdown block into html node #
+# Input:    block      - markdown block                            #
+#           block_type - type of markdown block                    #
+# Return:   HTMLNode                                               #
+####################################################################
+def block_to_html(block, block_type):
+    match (block_type):
+        case BlockType.PARAGRAPH:
+            lines = block.split('\n')
+            text = " ".join(line.strip() for line in lines if line.strip() != "")
+            return ParentNode("p", text_to_children(text))
+
+        case BlockType.HEADING:
+            text, level = prepare_heading_block(block)
+            return ParentNode(f"h{level}", text_to_children(text))
+
+        case BlockType.QUOTE:
+            text = prepare_quote_block(block)
+            return ParentNode("blockquote", text_to_children(text))
+
+        case BlockType.UNORDERED_LIST:
+            children = prepare_unordered_list_block(block)
+            return ParentNode("ul", children)
+
+        case BlockType.ORDERED_LIST:
+            children = prepare_ordered_list_block(block)
+            return ParentNode("ol", children)
+
+        case BlockType.CODE:
+            lines = block.split('\n')
+            text = '\n'.join(lines[1:-1]) + '\n'
+            text_node = TextNode(text, TextType.CODE)
+            code_html_node = text_to_html_node(text_node)
+            return ParentNode("pre", [ code_html_node ])
+
+        case _:
+            raise ValueError("Invalid block type!")
+
+###################################################################
+# Function: markdown_to_html_node - converts markdown string into #
+#                                   html nodes with children      #
+# Input:    markdown - markdown string to convert                 #
+# Return:   HTMLNode with children                                #
+###################################################################
+def markdown_to_html_node(markdown):
+    blocks = markdown_to_blocks(markdown)
+
+    html_children = []
+    outer_parent = ParentNode("div", html_children)
+
+    for block in blocks:
+        b_type = block_to_block_type(block)
+        html_node = block_to_html(block, b_type)
+        html_children.append(html_node)
+
+    return outer_parent
--- src/test_functions.py
+++ src/test_functions.py
@@ -1,7 +1,19 @@
 import unittest
 
-from functions import text_to_html_node
+from functions import (
+    text_to_html_node,
+    split_nodes_delimiter,
+    extract_markdown_images,
+    extract_markdown_links,
+    split_nodes_links,
+    split_nodes_images,
+    text_to_textnodes,
+    markdown_to_blocks,
+    block_to_block_type,
+    markdown_to_html_node
+)
 from textnode import TextNode, TextType
+from blocktype import BlockType
 
 class TestFunctions(unittest.TestCase):
     def test_text(self):
@@ -34,3 +46,203 @@ class TestFunctions(unittest.TestCase):
         self.assertTrue("alt" in html_node.props)
         self.assertEqual(html_node.props["src"], "picture.com")
         self.assertEqual(html_node.props["alt"], "This is an image node")
+
+    def test_split_code_node(self):
+        node = TextNode("This is text with a `code block` word", TextType.PLAIN)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(len(new_nodes), 3)
+        self.assertEqual(new_nodes[0], TextNode("This is text with a ", TextType.PLAIN))
+        self.assertEqual(new_nodes[1], TextNode("code block", TextType.CODE))
+        self.assertEqual(new_nodes[2], TextNode(" word", TextType.PLAIN))
+
+    def test_split_multi_code_node(self):
+        node = TextNode("This is text with a `code block` word. And another `one` here.", TextType.PLAIN)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(len(new_nodes), 5)
+        self.assertEqual(new_nodes[0], TextNode("This is text with a ", TextType.PLAIN))
+        self.assertEqual(new_nodes[1], TextNode("code block", TextType.CODE))
+        self.assertEqual(new_nodes[2], TextNode(" word. And another ", TextType.PLAIN))
+        self.assertEqual(new_nodes[3], TextNode("one", TextType.CODE))
+        self.assertEqual(new_nodes[4], TextNode(" here.", TextType.PLAIN))
+
+    def test_split_multi_code_nodes(self):
+        node = TextNode("This is text with a `code block` word. And another `one` here.", TextType.PLAIN)
+        node2 = TextNode("And here is even a `second` node itself!", TextType.PLAIN)
+        new_nodes = split_nodes_delimiter([node, node2], "`", TextType.CODE)
+        self.assertEqual(len(new_nodes), 8)
+        self.assertEqual(new_nodes[0], TextNode("This is text with a ", TextType.PLAIN))
+        self.assertEqual(new_nodes[1], TextNode("code block", TextType.CODE))
+        self.assertEqual(new_nodes[2], TextNode(" word. And another ", TextType.PLAIN))
+        self.assertEqual(new_nodes[3], TextNode("one", TextType.CODE))
+        self.assertEqual(new_nodes[4], TextNode(" here.", TextType.PLAIN))
+        self.assertEqual(new_nodes[5], TextNode("And here is even a ", TextType.PLAIN))
+        self.assertEqual(new_nodes[6], TextNode("second", TextType.CODE))
+        self.assertEqual(new_nodes[7], TextNode(" node itself!", TextType.PLAIN))
+
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with an [link to google](https://google.com)"
+        )
+        self.assertListEqual([("link to google", "https://google.com")], matches)
+
+    def test_split_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.PLAIN,
+        )
+        new_nodes = split_nodes_images([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.PLAIN),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.PLAIN),
+                TextNode(
+                    "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
+        )
+
+    def test_split_links(self):
+        node = TextNode(
+            "This is text with an [link1](https://i.imgur.com/zjjcJKZ.png) and another [link2](https://i.imgur.com/3elNhQu.png)",
+            TextType.PLAIN,
+        )
+        new_nodes = split_nodes_links([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.PLAIN),
+                TextNode("link1", TextType.LINK, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.PLAIN),
+                TextNode(
+                    "link2", TextType.LINK, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
+        )
+
+    def test_text_to_textnodes(self):
+        text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        nodes = text_to_textnodes(text)
+        self.assertListEqual(
+            [
+                TextNode("This is ", TextType.PLAIN),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with an ", TextType.PLAIN),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word and a ", TextType.PLAIN),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an ", TextType.PLAIN),
+                TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+                TextNode(" and a ", TextType.PLAIN),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
+            ],
+            nodes,
+        )
+
+    def test_markdown_to_blocks(self):
+        md = """
+This is **bolded** paragraph
+
+This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
+
+
+- This is a list
+- with items
+"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "This is **bolded** paragraph",
+                "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
+                "- This is a list\n- with items",
+            ],
+        )
+
+    def test_block_to_block_type_heading(self):
+        md = "### Heading at level 3"
+        self.assertEqual(
+            block_to_block_type(md),
+            BlockType.HEADING
+        )
+        md_fail = "##Invalid Heading, just paragraph."
+        b_type = block_to_block_type(md_fail)
+        self.assertNotEqual(b_type, BlockType.HEADING)
+        self.assertEqual(b_type, BlockType.PARAGRAPH)
+
+    def test_block_to_block_type_code(self):
+        md = """```
+Code block
+```"""
+        self.assertEqual(
+            block_to_block_type(md),
+            BlockType.CODE
+        )
+
+    def test_block_to_block_type_ulist(self):
+        md ="""- list entry 1
+- list entry 2
+- list entry 3"""
+
+        self.assertEqual(
+            block_to_block_type(md),
+            BlockType.UNORDERED_LIST
+        )
+
+    def test_block_to_block_type_olist(self):
+        md = """1. list entry 1
+2. list entry 2
+3. list entry 3"""
+
+        self.assertEqual(
+            block_to_block_type(md),
+            BlockType.ORDERED_LIST
+        )
+
+        md_fail = """2. list entry 1
+1. list entry 2
+3. list entry 3"""
+        self.assertNotEqual(
+            block_to_block_type(md_fail),
+            BlockType.ORDERED_LIST
+        )
+
+    def test_paragraphs(self):
+        md = """
+This is **bolded** paragraph
+text in a p
+tag here
+
+This is another paragraph with _italic_ text and `code` here
+
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
+        )
+
+    def test_codeblock(self):
+        md = """
+```
+This is text that _should_ remain
+the **same** even with inline stuff
+```
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><pre><code>This is text that _should_ remain\nthe **same** even with inline stuff\n</code></pre></div>",
+        )
